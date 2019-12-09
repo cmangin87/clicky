4ab9ce0bef4b87dc64db88fb2fe2c3c5e9fcb4de
Christopher Mangin <mangin.christopher@gmail.com>
2019-12-09 18:59:58 -0500

    logic initialized - state and randomization of array of cards

--- src/App.js
+++ src/App.js
@@ -7,3 +7,23 @@ import Jumbotron from './components/Jumbotron/Jumbotron';
 import Image from './components/Image/Image';
 import choices from './choices.json';
 import './App.css';
+
+class App extends Component {
+    state = {
+      choices,
+      clicked: [],
+      highScore: 0
+    };
+
+    componentDidMount() {
+        this.setState({ choices: this.randomize(this.state.choices) });
+    }
+
+    randomize = array => {
+        for (let i = array.length - 1; i > 0; i--) {
+          const j = Math.floor(Math.random() * (i + 1));
+          [array[i], array[j]] = [array[j], array[i]];
+        }
+    
+        return array;
+      };
