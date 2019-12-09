1c656a736903a9a6e3de4a32500505a9f9ec95c9
Christopher Mangin <mangin.christopher@gmail.com>
2019-12-09 18:55:17 -0500

    initialized imports in app.js

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+import Container from './components/Container/Container';
+import Row from './components/Grid/Row';
+import Column from './components/Grid/Column';
+import Card from './components/Card/Card';
+import Jumbotron from './components/Jumbotron/Jumbotron';
+import Image from './components/Image/Image';
+import choices from './choices.json';
+import './App.css';
--- src/components/Jumbotron/Jumbotron.js
+++ src/components/Jumbotron/Jumbotron.js
@@ -7,7 +7,6 @@ const Jumbtron = props => {
       jumbotron jumbotron-fluid p-2 text-center ${
         props.dark ? 'bg-dark text-light' : 'bg-light text-dark'
       } `}>
-      {/* TAKE STYLE OUT JUST TO MAKE A POINT */}
       <span className="m-3" style={{ fontSize: 30 }}>
         Score: {props.score}
       </span>
